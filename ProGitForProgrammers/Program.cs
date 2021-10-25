c4869378414f2918ba30999150067d8ad0fe9cf9
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-25 16:39:46 +0300

    Add informative line

--- ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/Program.cs
@@ -7,6 +7,7 @@ namespace progitforprogrammers
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("I just added this in Visual Studio");
         }
     }
 }
