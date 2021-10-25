10f40973b99fd0c8dd89fc82bb5265e18192b002
vytautas-dotcom <tumas.vytautas@gmail.com>
2021-10-25 22:31:57 +0300

    Add property property

--- ProGitForProgrammers/Book.cs
+++ ProGitForProgrammers/Book.cs
@@ -5,4 +5,6 @@ public class Book
     public string Title { get; set; }
     public List<string> Authors { get; set; }
     public DateTime PublicationDate { get; set; }
+
+    public string Property { get; set; }
 }
